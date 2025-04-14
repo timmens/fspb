519d47951f0be6a515b1730b49c981f9379f505c
Tim Mensinger <mensingertim@gmail.com>
2025-04-14 18:19:16 +0200

    Add tests for covariance module

--- src/fspb/covariance.py
+++ src/fspb/covariance.py
@@ -7,12 +7,33 @@ def calculate_covariance_on_grid(
     x: NDArray[np.float64],
     x_new: NDArray[np.float64],
 ) -> NDArray[np.float64]:
-    sigma_x = np.tensordot(x, x, axes=([0], [0])).transpose(1, 3, 2, 0) / x.shape[0]
+    """Calculate the covariance of the residuals on a grid.
 
-    sigma_x_inv = np.linalg.inv(sigma_x)
+    Args:
+        residuals: The residuals of the model. Has shape (n_samples, n_time_points).
+        x: The design matrix. Has shape (n_samples, n_features, n_time_points).
+        x_new: The new design matrix for which to calculate the covariance. Has shape
+            (n_features, n_time_points).
 
-    sigma_error = residuals.T @ residuals / len(residuals)
-
-    xt_sigma_x_inv_xt = np.einsum("is,stij,jt->st", x_new, sigma_x_inv, x_new)
-
+    """
+    sigma_x_inv = _calculate_sigma_inv(x)
+    sigma_error = _calculate_homoskedastic_error(residuals)
+    xt_sigma_x_inv_xt = _multiply_x_new_sigma_x_inv_x_newT(x_new, sigma_x_inv)
     return sigma_error * xt_sigma_x_inv_xt
+
+
+def _calculate_sigma_inv(x: NDArray[np.float64]) -> NDArray[np.float64]:
+    sigma_x = np.tensordot(x, x, axes=([0], [0])).transpose(1, 3, 2, 0) / x.shape[0]
+    return np.linalg.inv(sigma_x).astype(np.float64)
+
+
+def _calculate_homoskedastic_error(
+    residuals: NDArray[np.float64],
+) -> NDArray[np.float64]:
+    return residuals.T @ residuals / len(residuals)
+
+
+def _multiply_x_new_sigma_x_inv_x_newT(
+    x_new: NDArray[np.float64], sigma_x_inv: NDArray[np.float64]
+) -> NDArray[np.float64]:
+    return np.einsum("is,stij,jt->st", x_new, sigma_x_inv, x_new)
---
+++ tests/test_covariance.py
@@ -0,0 +1,83 @@
+from fspb.covariance import (
+    calculate_covariance_on_grid,
+    _calculate_sigma_inv,
+    _calculate_homoskedastic_error,
+    _multiply_x_new_sigma_x_inv_x_newT,
+)
+import pytest
+import numpy as np
+from numpy.testing import assert_array_equal
+
+
+@pytest.fixture
+def data():
+    x = np.stack([np.eye(3), 2 * np.eye(3)], axis=2)
+    x_new = 3 * np.eye(3)[:, :2]
+    residuals = np.array(
+        [
+            [-1, -2],
+            [0, 0],
+            [1, 2],
+        ]
+    )
+    return x, x_new, residuals
+
+
+def test_calculate_sigma_inv(data):
+    x, _, _ = data
+    result = _calculate_sigma_inv(x)
+    expected_shape = (2, 2, 3, 3)
+    expected = np.empty(expected_shape)
+    for s in (0, 1):
+        for t in (0, 1):
+            if s == t == 0:
+                arr = np.eye(3)
+            elif s == t == 1:
+                arr = np.eye(3) / 4
+            else:
+                arr = np.eye(3) / 2
+            expected[s, t] = 3 * arr  # 3 is the number of samples
+
+    assert result.shape == expected_shape
+    assert_array_equal(result, expected)
+
+
+def test_calculate_homoskedastic_error(data):
+    _, _, residuals = data
+    expected_shape = (2, 2)
+    expected = np.array(
+        [
+            [2 / 3, 4 / 3],
+            [4 / 3, 8 / 3],
+        ]
+    )
+    got = _calculate_homoskedastic_error(residuals)
+    assert got.shape == expected_shape
+    assert_array_equal(got, expected)
+
+
+def test_multiply_x_new_sigma_x_inv_x_newT(data):
+    x, x_new, _ = data
+    sigma_x_inv = _calculate_sigma_inv(x)
+    expected = np.array(
+        [
+            [27, 0],
+            [0, 6.75],
+        ]
+    )
+    got = _multiply_x_new_sigma_x_inv_x_newT(x_new, sigma_x_inv)
+    assert got.shape == expected.shape
+    assert_array_equal(got, expected)
+
+
+def test_calculate_covariance_on_grid(data):
+    x, x_new, residuals = data
+    expected = np.array(
+        [
+            [2 * 27 / 3, 0],
+            [0, 8 * 6.75 / 3],
+        ]
+    )
+    got = calculate_covariance_on_grid(residuals, x=x, x_new=x_new)
+    assert got.shape == expected.shape
+    assert_array_equal(got, expected)
