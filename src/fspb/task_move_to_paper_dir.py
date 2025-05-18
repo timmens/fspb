5f31e34a1442b0792947744557e3ff3d93391d4d
Tim Mensinger <mensingertim@gmail.com>
2025-05-18 15:25:32 +0200

    Update example outcomes figure

--- src/fspb/task_move_to_paper_dir.py
+++ src/fspb/task_move_to_paper_dir.py
@@ -11,25 +11,19 @@ from pytask import Product
 from typing import Annotated
 
 
-for covariance_type in ("stationary", "non_stationary"):
-    outcome_figure_path = BLD_FIGURES / f"outcomes_{covariance_type}.pdf"
-    to_path = PAPER_BLD / f"outcomes_{covariance_type}.pdf"
-
-    @pytask.task(id=str(covariance_type))
-    @pytask.mark.skipif(
-        not MOVE_RESULTS_TO_PAPER_DIR, reason="Not moving results to paper directory."
-    )
-    def task_move_outcome_figure(
-        outcome_figure_path: Path = outcome_figure_path,
-        to_path: Annotated[Path, Product] = to_path,
-    ) -> None:
-        shutil.copy(outcome_figure_path, to_path)
+@pytask.mark.skipif(
+    not MOVE_RESULTS_TO_PAPER_DIR, reason="Not moving results to paper directory."
+)
+def task_move_outcome_figure(
+    outcome_figure_path: Path = BLD_FIGURES / "outcomes.pdf",
+    to_path: Annotated[Path, Product] = PAPER_BLD / "outcomes.pdf",
+) -> None:
+    shutil.copy(outcome_figure_path, to_path)
 
 
 @pytask.mark.skipif(
     not MOVE_RESULTS_TO_PAPER_DIR, reason="Not moving results to paper directory."
 )
-@pytask.task(id=str(covariance_type))
 def task_move_band_figure(
     band_figure_path: Path = BLD_FIGURES / "band_seed_0.pdf",
     to_path: Annotated[Path, Product] = PAPER_BLD / "band.pdf",
--- src/fspb/task_visualize_outcome.py
+++ src/fspb/task_visualize_outcome.py
@@ -14,80 +14,88 @@ from fspb.simulation.model_simulation import (
 from pytask import Product
 
 PAPER_TEXT_WIDTH = 8.5 - 2  # us-letter width in inches minus margin
-FIG_FONT_SIZE = 10
+FIG_FONT_SIZE = 11
 
 
 def task_visualize_outcome(
     _script: Path = SRC / "simulation" / "model_simulation.py",
-    path_figure: Annotated[dict[str, Path], Product] = {
-        "stationary": BLD_FIGURES / "outcomes_stationary.pdf",
-        "non_stationary": BLD_FIGURES / "outcomes_non_stationary.pdf",
-    },
+    path_figure: Annotated[Path, Product] = BLD_FIGURES / "outcomes.pdf",
 ) -> None:
     """Save Figure XXX for paper."""
     data = _generate_outcome_figure_data()
-    figures = _create_outcome_figures(data)
-    for key, figure in figures.items():
-        figure.savefig(path_figure[key], bbox_inches="tight")
-        plt.close(figure)
-
-
-def _create_outcome_figure(
-    data: dict[str, NDArray[np.floating]],
-    key: str,
-    color: str,
-) -> plt.Figure:
-    """Create figure showing stationary and non-stationary outcomes."""
-    plt.style.use("seaborn-v0_8-whitegrid")
-    plt.rc("text", usetex=True)
-    plt.rc("font", family="serif", serif=["Computer Modern Roman"])
-
-    fig, ax = plt.subplots()
-
-    ax.plot(
-        data["time_grid"],
-        data[key],
-        color=color,
-        linewidth=0.9,
-        alpha=0.8,
-    )
-
-    ax.spines["top"].set_visible(False)
-    ax.spines["right"].set_visible(False)
-    ax.spines["left"].set_visible(False)
-    ax.spines["bottom"].set_visible(False)
-    ax.tick_params(labelsize=FIG_FONT_SIZE)
-    ax.grid(visible=True, linestyle="--", alpha=0.7)
-    ax.set_xlim(0, 1)
-    ax.set_ylim(-1.49, 1.49)
-    ax.set_xticks([0, 0.25, 0.5, 0.75, 1])
-
-    ax.set_xlabel("$t$", fontsize=FIG_FONT_SIZE)
-    fig.text(0, 0.50, "$Y_i(t)$", fontsize=FIG_FONT_SIZE, rotation=0)
-
-    fig.tight_layout(rect=(0, 0, 1, 1))
-    fig.set_size_inches(PAPER_TEXT_WIDTH, 1.5)
-    fig.subplots_adjust(hspace=0)
-    return fig
+    fig = _create_outcome_figures(data)
+    fig.savefig(path_figure, bbox_inches="tight")
 
 
 def _create_outcome_figures(
     data: dict[str, NDArray[np.floating]],
-) -> dict[str, plt.Figure]:
+) -> plt.Figure:
     """Create figure showing stationary and non-stationary outcomes."""
+
+    plt.style.use("seaborn-v0_8-whitegrid")
+    plt.rc("text", usetex=True)
+    plt.rc("font", family="serif", serif=["Computer Modern Roman"])
+
+    fig, axes = plt.subplots(
+        nrows=1,
+        ncols=2,
+        figsize=(PAPER_TEXT_WIDTH, 3),
+        sharex=True,
+        sharey=True,
+    )
+
     tableau_blue = "#5778a4"
     tableau_orange = "#e49444"
 
-    fig_stationary = _create_outcome_figure(
-        data, key="stationary_outcomes", color=tableau_blue
-    )
-    fig_non_stationary = _create_outcome_figure(
-        data, key="non_stationary_outcomes", color=tableau_orange
-    )
-    return {
-        "stationary": fig_stationary,
-        "non_stationary": fig_non_stationary,
+    plot_data = {
+        "stationary": {
+            "title": "(a) Stationary",
+            "ax": axes[0],
+            "color": tableau_blue,
+            "time_grid": data["time_grid"],
+            "data": data["stationary_outcomes"],
+        },
+        "non_stationary": {
+            "title": "(b) Non-stationary",
+            "ax": axes[1],
+            "color": tableau_orange,
+            "time_grid": data["time_grid"],
+            "data": data["non_stationary_outcomes"],
+        },
     }
+
+    for name, pdata in plot_data.items():
+        ax = pdata["ax"]
+
+        ax.plot(
+            pdata["time_grid"],
+            pdata["data"],
+            color=pdata["color"],
+            linewidth=0.9,
+            alpha=0.8,
+        )
+
+        ax.spines["top"].set_visible(False)
+        ax.spines["right"].set_visible(True)
+        ax.spines["left"].set_visible(True)
+        ax.spines["bottom"].set_visible(False)
+        ax.tick_params(labelsize=FIG_FONT_SIZE)
+        ax.grid(visible=True, linestyle="--", alpha=0.7)
+        ax.set_xlim(0, 1)
+        ax.set_ylim(-1.9, 1.95)
+        ax.set_xticks([0, 1 / 3, 2 / 3, 1])
+        ax.set_xticklabels(["$0$", "$1/3$", "$2/3$", "$1$"])
+        ax.set_yticks([-1, 0, 1])
+        ax.set_yticklabels(["$-1$", "$0$", "$1$"])
+        ax.set_xlabel("$t$", fontsize=FIG_FONT_SIZE)
+        ax.set_title(pdata["title"], fontsize=FIG_FONT_SIZE)
+
+    # fig.text(0, 0.50, "$Y_i(t)$", fontsize=FIG_FONT_SIZE, rotation=0)
+    # fig.tight_layout(rect=(0.02, 0, 1, 1))
+    fig.tight_layout(rect=(0, 0, 1, 1))
+    fig.set_size_inches(PAPER_TEXT_WIDTH, 1.5)
+    fig.subplots_adjust(hspace=0)
+    return fig
 
 
 def _generate_outcome_figure_data() -> dict[str, NDArray[np.floating]]:
